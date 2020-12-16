9053976a1b8570aba78064d20bcb6367ecc2a35b
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-16 15:28:41 +0100

    Create math.c

---
+++ math/math.c
@@ -0,0 +1,39 @@
+//  O3 Ozone Project Lab
+//  Math.c - addition, subtraction, multiplication, division.
+//  Created by Hudson Schumaker on 14.08.20.
+//  Copyright © 2020 SchumakerTeam. All rights reserved.
+
+const double pi = 3.141592;
+
+// Interger operations
+int add_int(int x0, int x1) {
+    return x0 + x1;
+}
+
+int add_3_int(int x0, int x1, int x2) {
+    return x0 + x1 + x2;
+}
+
+int add_4_int(int x0, int x1, int x2, int x3) {
+    return x0 + x1 + x2 + x3;
+}
+
+int sub_int(int x0, int x1) {
+    return x0 - x1;
+}
+
+int mult_int(int x0, int x1) {
+    return x0 * x1;
+}
+
+int div_int(int x0, int x1) {
+    return x0 / x1;
+}
+
+int inc_int(int x0) {
+    return x0++;
+}
+
+int dec_int(int x0) {
+    return x0--;
+}
