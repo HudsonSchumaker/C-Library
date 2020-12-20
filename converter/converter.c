eae4c496175b4be968ad465c93b925ca9cf117d4
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-20 16:34:27 +0100

    Update converter.c

--- converter/converter.c
+++ converter/converter.c
@@ -76,3 +76,15 @@ float fahrenheit_to_celsius(float f) {
     // (32°F − 32) × 5/9 = 0°C
     return (f - 32.0) * (5/9);
 }
+
+// Celsius to Kelvin
+float celsius_to_kelvin(float c) {
+    // 0°C + 273.15 = 273,15K
+    return c + 273.15;
+}
+
+// Kelvin to Celsius
+float kelvin_to_celsius(float k) {
+    // 0K − 273.15 = -273,1°C
+    return k - 273.15;
+}
