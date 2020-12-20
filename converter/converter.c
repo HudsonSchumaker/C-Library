c5e153125d615cb677f0e474c24997e0fc417583
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-20 16:51:52 +0100

    remove int, float, double to string methods.

--- converter/converter.c
+++ converter/converter.c
@@ -8,63 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Integer to char array(string)
-const char* int_to_string(int i) {
-    char str[12];// fit all integer numbers.
-    sprintf(str, "%d", i);
-    char *buffer = malloc(12);
-    strcpy(buffer, str);
-    return buffer;
-}
-
-// Float to char array(string)
-const char* float_to_string(float f) {
-    int afterpoint = 4;
-    int ipart = (int)f;               // extract integer part 
-    float fpart = f - (float)ipart;   // extract float part 
-    
-    char istr[8];
-    sprintf(istr, "%d", ipart);
-    
-    if (afterpoint != 0) { 
-        fpart = fpart * pow(10, afterpoint); 
-    }
-    
-    int fpart_no_dot = fpart;
-    char fstr[8];
-    sprintf(fstr, "%d", fpart_no_dot);
-   
-    char *buffer = malloc(16);
-    strcpy(buffer, istr);
-    strcat(buffer, ".");
-    strcat(buffer, fstr);
-    return buffer;
-}
-
-// Double to char array(string)
-const char* double_to_string(double d) {
-    int afterpoint = 6;
-    int ipart = (int)d;                 // extract integer part 
-    double dpart = d - (double)ipart;   // extract double part 
-    
-    char istr[8];
-    sprintf(istr, "%d", ipart);
-    
-    if (afterpoint != 0) { 
-        dpart = dpart * pow(10, afterpoint); 
-    }
-    
-    int dpart_no_dot = dpart;
-    char dstr[8];
-    sprintf(dstr, "%d", dpart_no_dot);
-   
-    char *buffer = malloc(16);
-    strcpy(buffer, istr);
-    strcat(buffer, ".");
-    strcat(buffer, dstr);
-    return buffer;
-}
-
 // Celsius to Fahrenheit
 float celsius_to_fahrenheit(float c) {
     // (0°C × 9/5) + 32 = 32°F
