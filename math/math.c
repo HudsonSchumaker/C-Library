72b1e98c4f27b919a4b8d905a54f5c4af24f2c5a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-25 19:55:56 +0000

    Update math.c

--- math/math.c
+++ math/math.c
@@ -46,9 +46,9 @@ int cube_int(int x0) {
     return x0 * x0 * x0;
 }
 
-int factorial(int n) {
-    int fact = 1;
-    for (int i = n; i > 1; --i) {
+int factorial(register int n) {
+    register int fact = 1;
+    for (register int i = n; i > 1; --i) {
         fact *= i;
     }
     return fact;
