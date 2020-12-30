eabde3b72e9b78078564901537aa3013261177d8
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-12-30 17:58:52 +0100

    Update files.c

--- file/files.c
+++ file/files.c
@@ -1,1 +1,14 @@
-
+//  O3 Ozone Project Lab
+//  files.c -  create methods
+//             read methods
+//             write methods
+//             close methods
+//             delete methods
+//
+//  Created by Hudson Schumaker on 14.08.20.
+//  Copyright © 2020 SchumakerTeam. All rights reserved.
+//
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
