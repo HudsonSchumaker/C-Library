3f733c9047bae42826b2376af2a232f732983db8
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-05-06 20:05:21 +0200

    Add files via upload

---
+++ geom/Point2D.h
@@ -0,0 +1,38 @@
+//
+//  Point2D.h
+//  hello
+//
+//  Created by Hudson Luiz Sales Schumaker on 06.05.21.
+//
+
+#ifndef POINT2D_H
+#define POINT2D_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Point2D {
+private:
+	double x;
+	double y;
+
+public:
+	Point2D();
+	Point2D(double x, double y);
+
+	void move(double x, double y);
+	void translate(double d);
+	void translate(double dx, double dy);
+
+	void setX(double x) { this->x = x; };
+	double getX() const { return x; };
+
+	void setY(double y) { this->y = y; };
+	double getY() const { return y; };
+
+	string toString() { return "Point[" + to_string(x) + ", " + to_string(y) + "]"; };
+};
+
+#endif /* POINT2D_H */
