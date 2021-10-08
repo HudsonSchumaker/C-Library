bed37ad6120b449a47d0de45ce3ddc16a986523b
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-10-09 00:11:18 +0200

    Create converter.h

---
+++ converter/converter.h
@@ -0,0 +1,22 @@
+#ifndef CONV_H
+#define CONV_H
+
+float celsius_to_fahrenheit(register float c);
+float fahrenheit_to_celsius(register float f);
+float celsius_to_kelvin(register float c);
+float kelvin_to_celsius(register float k);
+float kelvin_to_fahrenheit(register float k); 
+float fahrenheit_to_kelvin(register float f); 
+float km_to_miles(register float km);
+float mile_to_km(register float m); 
+float knot_to_km_h(register float k); 
+float km_h_to_knot(register float km); 
+float psi_to_bar(register float psi);
+float liter_to_gallon(register float l);
+float gallon_to_liter(register float g);
+float kilogram_to_pound(register float k);
+float pound_to_kilogram(register float p); 
+float watts_to_hp(register float w); 
+float hp_to_watts(register float hp); 
+
+#endif
