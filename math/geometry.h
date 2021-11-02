f33d484baf48fbb374def9a25f0dc51d7e4b2695
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-11-02 21:14:02 +0100

    Update geometry.h

--- math/geometry.h
+++ math/geometry.h
@@ -1,1 +1,14 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
 
+float area_square(register float s);
+float area_rectangle(register float l, register float w);
+float area_triangle(register float b, register float h);
+float area_parallelogram(register float b, register float h);
+float area_trapezoid(register float b, register float B, register float h);
+float area_circle(register float r);
+float area_sphere(register float r);
+float volume_cube(register float s);
+float volume_cone(register float r, register float h);
+float volume_cylinder(register float r, register float h);
+float volume_sphere(register float r);
