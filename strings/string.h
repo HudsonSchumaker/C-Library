8998a1259c8377fa284506a02e6747ac84866a79
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-11-12 07:13:54 +0100

    Update string.h

--- strings/string.h
+++ strings/string.h
@@ -18,5 +18,6 @@ const char* upper_case(char *str);
 const char* lower_case(char *str);
 const char* camel_case(char *str);
 const char* pascal_case(char *str);
+const char* capital_case(char *str);
 
 #endif
