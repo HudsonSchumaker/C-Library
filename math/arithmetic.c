e32a3f2d07b40493d6af8456cd3fc43320e08a28
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-11-28 17:35:29 +0100

    Create arithmetic.c

---
+++ math/arithmetic.c
@@ -0,0 +1,18 @@
+//  O3 Ozone Project Lab
+//  arithmetic.c
+//  Created by Hudson Schumaker on 14.08.20.
+//  Copyright © 2020 SchumakerTeam. All rights reserved.
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+float gcd(register float n1, register n2) {
+  int i, gcd;
+  for(i=1; i <= n1 && i <= n2; ++i) {
+        if(n1%i == 0 && n2%i == 0) {
+            gcd = i;
+        }
+    }
+    return gcd;  
+}
