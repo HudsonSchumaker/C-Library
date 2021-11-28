5440ac2b93258666b053fea3d0f138920f219fec
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-11-28 17:38:36 +0100

    Update arithmetic.c

--- math/arithmetic.c
+++ math/arithmetic.c
@@ -16,3 +16,14 @@ float gcd(register float n1, register n2) {
     }
     return gcd;  
 }
+
+float lcm(register n1, register n2) {
+  int gcd, lcm;
+  for (i = 1; i <= n1 && i <= n2; ++i) {
+      if (n1 % i == 0 && n2 % i == 0) {
+        gcd = i;
+      }
+  }
+  lcm = (n1 * n2) / gcd;
+  return lcm; 
+}
