c396e7ead48a1e40ede51f8673f5663c16e993c9
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-11-29 19:17:53 +0100

    Create files.h

---
+++ file/files.h
@@ -0,0 +1,10 @@
+//  O3 Ozone Project Lab
+//  Created by Hudson Schumaker on 14.08.20.
+//  Copyright © 2020 SchumakerTeam. All rights reserved.
+//
+
+FILE* create_file(char *path);
+void append_to_file_1(char *f, char *str);
+void append_to_file_2(FILE *f, char *str);
+void close_file(FILE *f);
+int delete_file(FILE *f);
