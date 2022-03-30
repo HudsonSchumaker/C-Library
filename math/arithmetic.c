30fc83950ecf14f7cbf5bd9c8d6ee9ce4f757406
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-03-30 19:26:25 +0200

    Update arithmetic.c

--- math/arithmetic.c
+++ math/arithmetic.c
@@ -3,27 +3,26 @@
 //  Created by Hudson Schumaker on 14.08.20.
 //  Copyright © 2020 SchumakerTeam. All rights reserved.
 
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include "arithmetic.h"
 
-float gcd(register float n1, register n2) {
-  int i, gcd;
-  for(i=1; i <= n1 && i <= n2; ++i) {
-        if(n1%i == 0 && n2%i == 0) {
+float gcd(register float n1, register float n2) {
+    float gcd = 0.0f;
+    for (float i = 1.0f; i <= n1 && i <= n2; ++i) {
+        if(fmodf(n1, i) == 0 && fmodf(n2, i) == 0) {
             gcd = i;
         }
     }
-    return gcd;  
+    return gcd;
 }
 
 float lcm(register float n1, register float n2) {
-  int gcd, lcm;
-  for (i = 1; i <= n1 && i <= n2; ++i) {
-      if (n1 % i == 0 && n2 % i == 0) {
-        gcd = i;
-      }
-  }
-  lcm = (n1 * n2) / gcd;
-  return lcm; 
+    float gcd = 0.0f;
+    float lcm = 0.0f;
+    for (float i = 1.0f; i <= n1 && i <= n2; ++i) {
+        if (fmodf(n1, i) == 0 && fmodf(n2, i) == 0) {
+            gcd = i;
+        }
+    }
+    lcm = (n1 * n2) / gcd;
+    return lcm;
 }
