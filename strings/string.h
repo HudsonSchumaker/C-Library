b1143c5cc72d2480b721a7df20761f1abe7ad8cb
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-11-20 18:51:35 +0100

    Update string.h

--- strings/string.h
+++ strings/string.h
@@ -1,7 +1,7 @@
 //  O3 Ozone Project Lab
 //  string.h
 //  Created by Hudson Schumaker 
-//  Copyright © 2020 SchumakerTeam. All rights reserved.
+//  Copyright © 2020 SchumakerTeam. All rights reserved
 
 #ifndef CONVERTER_H
 #define CONVERTER_H
