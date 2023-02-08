93f9e016524a63cba26bf195bdd2459ffd9a650a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-02-08 18:25:13 +0100

    Update vec3.h

--- physics/vec3.h
+++ physics/vec3.h
@@ -6,12 +6,11 @@ typedef struct {
 } vec3_t;
 
 float vec3_maginitude(vec3_t v);
+float vec3_dot(vec3_t a, vec3_t b);
 vec3_t vec3_add(vec3_t a, vec3_t b);
 vec3_t vec3_sub(vec3_t a, vec3_t b);
 vec3_t vec3_mul(vec3_t v, float factor);
 vec3_t vec3_div(vec3_t v, float factor);
 vec3_t vec3_cross(vec3_t a, vec3_t b);
-float vec3_dot(vec3_t a, vec3_t b);
-
 
 #endif
