86524fb51cb0cf5478a94e94dc2f79e2f54758f2
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-02-08 18:18:12 +0100

    Update vec2.h

--- physics/vec2.h
+++ physics/vec2.h
@@ -1,19 +1,10 @@
 #ifndef VEC2_H
 #define VEC2_H
 
-struct Vec2 {
+typedef struct {
     float x;
     float y;
+} vec2_t;
 
-    Vec2();
-    Vec2(float x, float y);
-    
-    ~Vec2() = default;
 
-    void Add(const Vec2& v);                 // v1.Add(v2)
-    void Sub(const Vec2& v);                 // v1.Sub(v2)
-    void Scale(const float n);               // v1.Scale(n)
-    Vec2 Rotate(const float angle) const;    // v1.Rotate(angle)
-
-    float Magnitude() const;                 // v1.Magnitude()
-    float MagnitudeSquared() const;          // v1.MagnitudeSquared()
+#endif
