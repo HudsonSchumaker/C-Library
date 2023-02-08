fea3d1b474f4612689358d24bf36bbc855e43391
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-02-08 18:21:27 +0100

    Update vec2.h

--- physics/vec2.h
+++ physics/vec2.h
@@ -6,7 +6,7 @@ typedef struct {
     float y;
 } vec2_t;
 
-float vec2_length(vec2_t v);
+float vec2_magnitude(vec2_t v);
 vec2_t vec2_add(vec2_t a, vec2_t b);
 vec2_t vec2_sub(vec2_t a, vec2_t b);
 vec2_t vec2_mul(vec2_t v, float factor);
