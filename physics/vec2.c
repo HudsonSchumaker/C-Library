f41df3571cb5666b42fb809f3ccd7baedb071710
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2023-02-10 12:34:37 +0100

    Update vec2.c

--- physics/vec2.c
+++ physics/vec2.c
@@ -4,3 +4,11 @@
 float vec2_maginitude(vec2_t v) {
     return sqrt(v.x * v.x + v.y * v.y);
 }
+
+vec2_t vec2_add(vec2_t a, vec2_t b) {
+    vec2_t result = {
+        .x = a.x + b.x,
+        .y = a.y + b.y
+    };
+    return result;
+}
