69a44453aabd38d701530212a27ff918f93a84a8
HudsonSchumaker <hudson.schumaker@me.com>
2023-02-12 11:43:15 +0100

    vec3 rotate x

--- physics/vec3.h
+++ physics/vec3.h
@@ -13,5 +13,6 @@ vec3_t vec3_mul(vec3_t v, float factor);
 vec3_t vec3_div(vec3_t v, float factor);
 vec3_t vec3_cross(vec3_t a, vec3_t b);
 void vec3_normalize(vec3_t* v);
+vec3_t vec3_rotate_x(vec3_t v, float angle);
 
 #endif
