ef31b7952d8ca8b401580a9058a912c70ced1af1
HudsonSchumaker <hudson.schumaker@me.com>
2023-02-12 11:38:53 +0100

    vec3 mul

--- physics/vec3.c
+++ physics/vec3.c
@@ -21,3 +21,12 @@ vec3_t vec3_sub(vec3_t a, vec3_t b) {
     };
     return result;
 }
+
+vec3_t vec3_mul(vec3_t v, float factor) {
+    vec3_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor,
+        .z = v.z * factor
+    };
+    return result;
+}
