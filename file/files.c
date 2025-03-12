d8808bd8b027fb3247800eb770381fea7186c8c1
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2025-03-12 19:04:19 +0100

    change code style

--- file/files.c
+++ file/files.c
@@ -16,27 +16,27 @@
 #define APP_READ_WRITE "a+"
 
 
-FILE* create_file(char *path) {
-    FILE *fp;
+FILE* create_file(char* path) {
+    FILE* fp;
     fp = fopen(path, TRUNC_READ_WRITE);
     return fp;
 }
 
-void append_to_file_1(char *f, char *str) {
-    FILE *fp;
+void append_to_file_1(char* f, char* str) {
+    FILE* fp;
     fp = fopen(f, APPEND);
     fprintf(fp, str);
     close_file(fp);
 }
 
-void append_to_file_2(FILE *f, char *str) {
+void append_to_file_2(FILE* f, char* str) {
     fprintf(f, str);
 }
 
-void close_file(FILE *f) {
+void close_file(FILE* f) {
     fclose(f);
 }
 
-int delete_file(FILE *f) {
+int delete_file(FILE* f) {
    return remove(f);
 }
