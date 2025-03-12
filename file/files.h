04729a3011fd99b80ed5d0f2a9a911657e7dadd9
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2025-03-12 19:05:04 +0100

    Update files.h

--- file/files.h
+++ file/files.h
@@ -12,10 +12,10 @@
 // have to declare this definition to user the functions as "override" functions 
 #define append_to_file(f, str) _Generic(f, char*:append_to_file_1 , FILE*: append_to_file_2)(f, str)
 
-FILE* create_file(char *path);
-void append_to_file_1(char *f, char *str);
-void append_to_file_2(FILE *f, char *str);
-void close_file(FILE *f);
-int delete_file(FILE *f);
+FILE* create_file(char* path);
+void append_to_file_1(char* f, char* str);
+void append_to_file_2(FILE* f, char* str);
+void close_file(FILE* f);
+int delete_file(FILE* f);
 
 #endif /* file_h */
